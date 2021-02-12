26eefafb4c0a66af0696fed0c74c5f973ef634ae
Thiago Vacari <vacari.thiago@gmail.com>
2021-02-11 21:15:36 -0300

    Adicionado scripts com inserts do banco

---
+++ scripts_banco/categoria_202102112114.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.categoria (descricao) VALUES
+	 ('Entregues'),
+	 ('Lançamentos');
---
+++ scripts_banco/tipo_202102112114.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.tipo (descricao) VALUES
+	 ('Residencial'),
+	 ('Comercial');
